665cc9aab12ef56006c39c9cdf8d3b8db4fc93ba
nista2121 <stanislas.mbengue@snhu.edu>
2021-04-30 13:46:34 -0400

    Initial Commit, Testing the application setup

---
+++ BookStoreApplication/src/com/BookStoreApplication/View/view.java
@@ -0,0 +1,11 @@
+package com.BookStoreApplication.View;
+
+public class view {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello to our Book Store, How can I help you?");
+
+	}
+
+}
---
+++ BookStoreApplication/src/module-info.java
@@ -0,0 +1,2 @@
+module BookStoreApplication {
+}
